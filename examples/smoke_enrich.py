03cb9835850d82e64ca9076676fd3dd6a835cf8c
Ding-Yang <199928ltyos@gmail.com>
2025-12-18 11:32:47 +0800

    refactor: update smoke test

--- examples/smoke_enrich.py
+++ examples/smoke_enrich.py
@@ -1,10 +1,17 @@
-from enrichrag.core.enrich import enrich
+from enrichrag.core.enricher import GeneEnricher
+from enrichrag.prompts.generator import PromptGenerator
 
-genes = ["TP53", "KRAS", "EGFR", "BRAF"]
+# --- 使用範例 ---
+genes = ["TP53", "KRAS", "EGFR", "TP63", "MYC", "PTEN", "RAF", "AKT"]
 
-res = enrich(genes)
+enricher = GeneEnricher(genes).run_enrichment().filter(pval_threshold=0.01)
+print(enricher.get_top_terms("GO", n=5))
 
-print(res.keys())
-print(len(res["results"]["GO"]), "GO terms")
-print(len(res["results"]["KEGG"]), "KEGG terms")
-print(res["results"])
+generator = PromptGenerator(template_path="/home/blue/code/EnrichRAG/enrichrag/prompts/templates/enrichment_analysis.yaml")
+final_insight_prompt = generator.build_enrichment_prompt(
+    gene_list=enricher.gene_list,
+    filtered_results=enricher.filtered_results,
+    disease="癌症"
+)
+
+print(final_insight_prompt)
